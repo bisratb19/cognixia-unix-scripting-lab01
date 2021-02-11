41322663e14f868f7843811d9595325b79aa51b9
EC2 Default User <ec2-user@ip-172-31-48-194.ec2.internal>
2021-02-11 20:50:02 +0000

    lab01.sh v0.04

--- lab01.sh
+++ lab01.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
+REPORT_DATE=$(date +"%Y-%m-%d-%T")
+REPORT_FILE='report_'$REPORT_DATE'.log'
+
 # 1
 
-printf 'HOSTNAME: '$HOSTNAME > report_file.txt
-printf 'IP address: 'ifconfig | cut -d ' ' -f10 | head -n 2 | tail -n 1 >> report_file.txt
-printf 'Free memory: 'free -mh | awk '/Mem:/ {print $4}' >> report_file.txt
-printf 'Number of CPU cores: 'lscpu | awk '/CPU/ {print $2}' | head -n 2 | tail -n 1 >> report_file.txt
+printf 'HOSTNAME: '$HOSTNAME > $REPORT_FILE
+printf 'IP address: 'ifconfig | cut -d ' ' -f10 | head -n 2 | tail -n 1 >> $REPORT_FILE
+printf 'Free memory: 'free -mh | awk '/Mem:/ {print $4}' >> $REPORT_FILE
+printf 'Number of CPU cores: 'lscpu | awk '/CPU/ {print $2}' | head -n 2 | tail -n 1 >> $REPORT_FILE
 
 # 2
-
+printf 'Number of processes running: 'ps -aux | wc -l >> $REPORT_FILE
 
 # 3
+printf 'Enter username to check: '
+read REPORT_USERNAME
 
+USER_CHECK=$(sed -n '/^$REPORT_USERNAME/p' /etc/passwd | wc -l)
+
+if [ $USER_CHECK > 0 ]; then
+	USER_CHECK_OUTPUT='User $REPORT_USERNAME is found in /etc/passwd'
+else
+	USER_CHECK_OUTPUT='User $REPORT_USERNAME is not found in /etc/passwd'
+fi
+
+printf $USER_CHECK_OUTPUT >> $REPORT_FILE
 
 # print report
-cat report_file.txt
+printf 'Log file name: '$REPORT_FILE
+cat $REPORT_FILE
