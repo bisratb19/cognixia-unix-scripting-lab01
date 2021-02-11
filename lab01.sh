bc5c49ad566595877ddf46822d566b6cca505594
EC2 Default User <ec2-user@ip-172-31-48-194.ec2.internal>
2021-02-11 20:59:00 +0000

    lab01.sh v0.05

--- lab01.sh
+++ lab01.sh
@@ -6,12 +6,12 @@ REPORT_FILE='report_'$REPORT_DATE'.log'
 # 1
 
 printf 'HOSTNAME: '$HOSTNAME > $REPORT_FILE
-printf 'IP address: 'ifconfig | cut -d ' ' -f10 | head -n 2 | tail -n 1 >> $REPORT_FILE
-printf 'Free memory: 'free -mh | awk '/Mem:/ {print $4}' >> $REPORT_FILE
-printf 'Number of CPU cores: 'lscpu | awk '/CPU/ {print $2}' | head -n 2 | tail -n 1 >> $REPORT_FILE
+printf 'IP address: '$(ifconfig | cut -d ' ' -f10 | head -n 2 | tail -n 1) >> $REPORT_FILE
+printf 'Free memory: '$(free -mh | awk '/Mem:/ {print $4}') >> $REPORT_FILE
+printf 'Number of CPU cores: '$(lscpu | awk '/CPU/ {print $2}' | head -n 2 | tail -n 1) >> $REPORT_FILE
 
 # 2
-printf 'Number of processes running: 'ps -aux | wc -l >> $REPORT_FILE
+printf 'Number of processes running: '$(ps -aux | wc -l) >> $REPORT_FILE
 
 # 3
 printf 'Enter username to check: '
